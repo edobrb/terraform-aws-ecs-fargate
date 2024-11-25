cc50737c03856bc92e1fa7588232e2a80637542f
Edoardo Barbieri <edoardo@twinlogix.com>
2024-11-25 16:56:35 +0100

    fix

--- main.tf
+++ main.tf
@@ -34,7 +34,6 @@ resource "aws_iam_role_policy" "ssm_execution" {
 }
 
 resource "aws_iam_role_policy" "kms_execution" {
-  count  = length(local.image_pull_credentials) > 0 ? 1 : 0
   name   = "${var.name_prefix}-task-kms"
   role   = aws_iam_role.execution.id
   policy = data.aws_iam_policy_document.task_ecs_kms_policy.json
